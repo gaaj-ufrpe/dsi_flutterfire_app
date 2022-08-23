9590394e5b1735a60a18643fc7fc5b911beb74e9
gaaj-ufrpe <gabriel.alves@ufrpe.br>
2022-08-23 09:02:02 -0300

    Ajustes no app de contador.

--- README.md
+++ README.md
@@ -1,16 +1,70 @@
-# dsi_flutterfire_app
+# Descrição
 
-A new Flutter project with FlutterFire.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
+This project is a starting point for a Flutter application with Firebase Firestor.
 
 A few resources to get you started if this is your first Flutter project:
 
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [Firebase w/ Flutter](https://firebase.google.com/docs/android/setup) 
+- [Firestore w/ Flutter](https://firebase.google.com/docs/flutter/setup?platform=android) 
 
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+# Instalando
+
+- Leia: https://firebase.google.com/docs/flutter/setup?platform=android
+- Baixe o flutter, descompacte em um diretório e adicione o caminho <code>[flutter_home]\bin</code> no path.
+- Abra o VSCode. Abra o terminal e digite <code>flutter doctor</code>.
+- Caso esteja dando algum erro, corrija. Alguns erros comuns são referentes à necessidade de reinstalar ou atualizar programas como o Visual Studio ou o Java. No caso do Visual Studio, ainda é necessário instalar o componente "Desktop development with C++".
+- Instale o plugin do Flutter no VSCode (e as dependências que forem solicitadas, como o Dart).
+- Abra o VSCode e baixe o código do repositório (https://github.com/gaaj-ufrpe/dsi_flutterfire_app).
+- Abra o arquivo <code>main.dart</code> e rode o programa (<code>Executar > Executar sem Depuração (CTRL+F5)</code>). Pode ser necessário configurar um emulador android. Fique atento à instalação atualizada do JDK do Java e do Flutter. Se necessário ajuste o arquivo build.gradle para a versão específica do SDK do android.
+
+# Getting Started
+
+
+
+
+///LEIA:
+///
+///
+///1) Para saber o nome do app, abra o arquivo 'android/app/build.gradle'
+/// e procure a propriedade 'applicationId'. Por padrão o nome é:
+/// [pacote completo].[nome do projeto]
+/// Ex.: br.ufrpe.gaaj.dsi_flutterfire_app
+///
+///2) No Flutter Console, na página de "Visão Geral do Projeto", abaixo do nome do
+/// projeto, há um botão "+Adicionar app". É nele que se deve clicar para liberar
+/// o acesso do app ao firebase.
+///
+///3) Copiar o arquivo gerado para 'android/app'
+///
+///4) Ajustar o arquivo 'android/build.gradle' conforme orientação do Console do Firebase;
+///
+///5) Ajustar o arquivo 'android/app/build.gradle' conforme orientação do Console do Firebase.
+///Tenha atenção para acessar a opção Java ou Kotlin conforme a linguagem utilizada no projeto.
+///
+///6) A aba de problemas do AndroidStudio, pode acusar alguma incompatibilidade de
+/// versões do Kotlin ou outra lib. Faça os ajustes recomendados.
+/// Ex.: neste projeto foi necessário alterar a propriedade 'ext.kotlin_version'
+/// para a versão '1.5.31' no arquivo 'android/build.gradle'
+///
+///7) Acesse https://firebase.flutter.dev/ procure a lib 'firebase_core' e
+///Acesse a pagina 'Installing', copie o código indicado para ser adicionado no
+///pubspec.yaml e cole lá. Atenção para usar as últimas versões.
+///Os tutoriais da internet geralmente utilizam versões mais antigas.
+///Faça a mesma coisa para o plugin 'cloud_firestore'
+///(No tutorial do vídeo abaixo, utiliza-se o Firebase Realtime e nã
+///o o Firestore).
+///
+///
+///DÚVIDAS? VEJA:
+///https://www.youtube.com/watch?v=EXp0gq9kGxI
+///
+/// ADICIONAL:
+/// https://firebase.flutter.dev/docs/firestore/usage/
+/// https://medium.com/firebase-tips-tricks/how-to-use-cloud-firestore-in-flutter-9ea80593ca40
+///
--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,32 @@
-import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-const errorTitleStyle = TextStyle(color: Colors.yellow, fontSize: 20.0);
+const errorTitleStyle = TextStyle(color: Color.fromARGB(255, 255, 59, 59), fontSize: 20.0);
 const errorDescStyle = TextStyle(
-    color: Colors.yellow, fontStyle: FontStyle.italic, fontSize: 16.0);
-const exceptionTitleStyle = TextStyle(color: Colors.yellow, fontSize: 20.0);
+    color: Color.fromARGB(255, 255, 59, 59), fontStyle: FontStyle.italic, fontSize: 16.0);
+const exceptionTitleStyle = TextStyle(color: Color.fromARGB(255, 255, 59, 59), fontSize: 20.0);
 const exceptionDescStyle =
     TextStyle(color: Colors.red, fontStyle: FontStyle.italic, fontSize: 16.0);
 const textTitleStyle = TextStyle(fontSize: 20.0);
 const textDescStyle = TextStyle(fontSize: 16.0);
 
-Widget _buildLoading() {
-  return Center(
-    child: Wrap(
-      alignment: WrapAlignment.center,
-      children: const [
-        CircularProgressIndicator(),
-        SizedBox(width: 10),
-        Text('Loading...', style: textTitleStyle),
-      ],
-    ),
-  );
-}
+Widget _buildLoading() => Center(
+  child: Wrap(
+    alignment: WrapAlignment.center,
+    children: const [
+      CircularProgressIndicator(),
+      SizedBox(width: 10),
+      Text('Loading...', style: textTitleStyle),
+    ],
+  ),
+);
 
-///LEIA:
-///https://firebase.google.com/docs/flutter/setup?platform=android
-///
-///1) Para saber o nome do app, abra o arquivo 'android/app/build.gradle'
-/// e procure a propriedade 'applicationId'. Por padrão o nome é:
-/// [pacote completo].[nome do projeto]
-/// Ex.: br.ufrpe.gaaj.dsi_flutterfire_app
-///
-///2) No Flutter Console, na página de "Visão Geral do Projeto", abaixo do nome do
-/// projeto, há um botão "+Adicionar app". É nele que se deve clicar para liberar
-/// o acesso do app ao firebase.
-///
-///3) Copiar o arquivo gerado para 'android/app'
-///
-///4) Ajustar o arquivo 'android/build.gradle' conforme orientação do Console do Firebase;
-///
-///5) Ajustar o arquivo 'android/app/build.gradle' conforme orientação do Console do Firebase.
-///Tenha atenção para acessar a opção Java ou Kotlin conforme a linguagem utilizada no projeto.
-///
-///6) A aba de problemas do AndroidStudio, pode acusar alguma incompatibilidade de
-/// versões do Kotlin ou outra lib. Faça os ajustes recomendados.
-/// Ex.: neste projeto foi necessário alterar a propriedade 'ext.kotlin_version'
-/// para a versão '1.5.31' no arquivo 'android/build.gradle'
-///
-///7) Acesse https://firebase.flutter.dev/ procure a lib 'firebase_core' e
-///Acesse a pagina 'Installing', copie o código indicado para ser adicionado no
-///pubspec.yaml e cole lá. Atenção para usar as últimas versões.
-///Os tutoriais da internet geralmente utilizam versões mais antigas.
-///Faça a mesma coisa para o plugin 'cloud_firestore'
-///(No tutorial do vídeo abaixo, utiliza-se o Firebase Realtime e nã
-///o o Firestore).
-///
-///
-///DÚVIDAS? VEJA:
-///https://www.youtube.com/watch?v=EXp0gq9kGxI
-///
-/// ADICIONAL:
-/// https://firebase.flutter.dev/docs/firestore/usage/
-/// https://medium.com/firebase-tips-tricks/how-to-use-cloud-firestore-in-flutter-9ea80593ca40
-///
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyFirebaseApp());
-}
-
-class MyFirebaseApp extends StatefulWidget {
-  const MyFirebaseApp({Key? key}) : super(key: key);
-
-  // Create the initialization Future outside of `build`:
-  @override
-  _MyFirebaseAppState createState() => _MyFirebaseAppState();
-}
-
-class _MyFirebaseAppState extends State<MyFirebaseApp> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return _buildAppError(snapshot.error);
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const MyApp();
-        }
-
-        return _buildLoading();
-      },
-    );
-  }
-
-  Directionality _buildAppError(Object? error) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Center(
-        child: Wrap(
-          children: [
-            const Text('Error:', style: errorTitleStyle),
-            Text('$error', style: errorDescStyle),
-          ],
-        ),
-      ),
-    );
-  }
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -124,11 +35,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DSI Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'DSI Demo Home'),
     );
   }
 }
@@ -143,21 +54,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  CollectionReference<Map<String, dynamic>> get contadores =>
-      FirebaseFirestore.instance.collection('contadores');
+  late DocumentReference<Map<String, dynamic>> docRef;
 
-  //O método 'doc()' deve receber o id do documento que está no Firebase.
-  DocumentReference<Map<String, dynamic>> get docRef =>
-      contadores.doc('OqzaUV6COKypNVufZz9L');
+  @override
+  void initState() {
+    super.initState();
+    CollectionReference<Map<String, dynamic>> contadores = 
+      FirebaseFirestore.instance.collection('contadores');
+    docRef = contadores.doc('OqzaUV6COKypNVufZz9L');
+  }
 
   void _incrementCounter() async {
     DocumentSnapshot docSnap = await docRef.get();
     int counter = docSnap.get('valor');
-    counter++;
-    docRef.set(
-      {'valor': counter},
-      SetOptions(merge: true),
-    );
+    docRef.set({'valor': counter+1});
     setState(() {});
   }
 
@@ -183,14 +93,12 @@ class _MyHomePageState extends State<MyHomePage> {
         if (snapshot.hasError) {
           return _buildCounterError();
         }
-
         if (snapshot.connectionState == ConnectionState.done) {
           DocumentSnapshot<Map<String, dynamic>> docSnap =
               snapshot.data! as DocumentSnapshot<Map<String, dynamic>>;
           int counter = docSnap.get('valor');
           return _buildCounterDone(counter);
         }
-
         return _buildLoading();
       },
     );
@@ -202,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           const Text(
-            'You have pushed the button this many times:',
+            'Contador:',
             style: textDescStyle,
           ),
           Text(
